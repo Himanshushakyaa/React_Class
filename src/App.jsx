347da38d5970340dd57335d2ba922cc118e0f9c1
Himanshu <hs3928782@gmail.com>
2023-11-18 16:43:25 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+<h1 className="text-3xl font-bold underline">Hello Primathon</h1>
+</>
+  )
 };
 export default App;
